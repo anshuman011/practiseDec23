a00fd5a945683cd20393cd491ba33434c5cc1667
anshuman001 <infoanshuman001@gmail.com>
2023-12-30 15:23:33 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,17 @@
-// import logo from './logo.svg';
 import './App.css';
-import OneRepeatedTask from './AllPractiseFiles/OneRepeatedTask';
-import { Program1, Program2, Program3, Program5, Program6 } from './AllPractiseFiles/ReactInterviewQuestionsDec';
-import { TwoStopWatch, ObjectIterationsPractise, ArrayIterationsPractise } from './AllPractiseFiles/Two';
-import { SomeCoolEffects1 } from './AllPractiseFiles/somecooleffects';
+// import OneRepeatedTask from './AllPractiseFiles/OneRepeatedTask';
+// import { Program1, Program2, Program3, Program5, Program6 } from './AllPractiseFiles/ReactInterviewQuestionsDec';
+// import { TwoStopWatch, ObjectIterationsPractise, ArrayIterationsPractise } from './AllPractiseFiles/Two';
+// import { SomeCoolEffects1 } from './AllPractiseFiles/somecooleffects';
+import './AllPractiseFiles/javascriptPRACTISEquestions';
+import './AllPractiseFiles/jsPractiseNamasteJS';
+import './WissenPractise/wissenJsProgram';
 
 function App() {
   return (
     <div>
-      {/* <Program1 /> */}
-      {/* <Program2 /> */}
-      {/* <Program3 /> */}
-      <Program5 />
-      <Program6/>
-      <SomeCoolEffects1 />
     </div>
 
-    // <div style={{backgroundColor:"green", textAlign:"center"}}>
-    //   <OneRepeatedTask /> 
-    //   <TwoStopWatch />
-    //   <ObjectIterationsPractise />
-    //   <ArrayIterationsPractise />
-    //   <img src='/images/image_from_public_folder.jpg' alt= "image_from_public_folder" style={{height:"100px", width :"100px"}}/>
-    // </div>
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
